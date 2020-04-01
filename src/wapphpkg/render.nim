e6eac6c39e022570be985dc2e009c1ec3592bfad
Ryan Cotter <ryanc@lastyard.com>
2020-04-01 20:47:46 +0800

    Integrated moustachu for templating

--- src/wapphpkg/render.nim
+++ src/wapphpkg/render.nim
@@ -3,60 +3,55 @@ import models
 import tables
 import strutils
 import os
+import moustachu
 
 const STATIC_DIR = "./public/compiled"
 
-proc buildHeader(): string =
-  result.add """<!DOCTYPE html><html><head>"""
-  result.add("""<title>$#</title>""")
+var baseTpl ="""
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>{{title}}</title>
+    {{#css_links}}
+      <link rel="stylesheet" href="http://0.0.0.0:5000/{{.}}" />
+    {{/css_links}}
+  </head>
+  <body>
+    <div class="grid-12">
+      <div class="col-2"></div>
+      <div class="col-8">
+        <header><h2>{{title}}</h2></header>
+        <article>{{main_content}}</article>
+        <footer></footer>
+      </div>
+      <div class="col-2"></div>
+    </div>
+  </body>
+</html>
+"""
 
+proc buildCssLinks(): seq[string] =
   for css_file in os.walkFiles(STATIC_DIR / "*.css"):
     let (dir, css_filename, extension) = css_file.splitFile()
-    echo "Adding $#" % css_filename
-    result.add("""<link rel="stylesheet" href="http://0.0.0.0:5000/$#" />""" % (dir.split("/")[^1] / css_filename & extension))
-
-  result.add("""</head>""")
-
-let HEADER = buildHeader()
-
-type
-  TPL = ref object of RootObj
-    header: string
-    footer: string
-
-proc newTpl(): TPL =
-  new result
-  result.header = """<header><h2>$#</h2></header><body>"""
-  result.footer = """<footer></footer>"""
-
-proc renderPage(page: Page, tpl = newTpl()): string =
-  ## Render a Page object
-
-  result.add("""<body><div class="grid-12"><div class="col-2"></div><div class="col-8">""")
-  result.add(tpl.header % page.title)
-
-  result.add("""<article>$#</article>""" % page.content)
-
-  result.add(tpl.footer)
-  result.add("""</div><div class="col-2"></div></div></body>""")
-
-proc render404(tpl = newTpl()): string =
-  result.add(tpl.header % "Post doesn't exist")
-  result.add("""<div><article>$#</article></div>""" % "Post doesn't exist")
-  result.add(tpl.footer)
+    echo "Adding " & css_filename
+    result.add(dir.split("/")[^1] / css_filename & extension)
 
 proc render*(id: int): string =
   withDb:
 
     try:
       let page = Page.getOne(id)
-      result.add(HEADER % page.title)
-      echo "Added header: " & HEADER
-      result.add(renderPage(page))
+      var context: Context = newContext()
+      context["title"] = page.title
+      context["css_links"] = buildCssLinks()
+      context["main_content"] = page.content
+      result = render(baseTpl, context)
+      echo result
     except KeyError:
-      result.add(HEADER % "Post Doesn't Exist")
-      result.add(render404())
-
-    result.add("""</html>""")
+      var context: Context = newContext()
+      context["title"] = "Can't find post"
+      context["css_links"] = buildCssLinks()
+      context["main_content"] = "Post doesn't exist"
+      result = render(baseTpl, context)
 
 export models
